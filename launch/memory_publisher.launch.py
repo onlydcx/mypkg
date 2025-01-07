2ce19deb49c4e6ac83320e28d33aeaf5df671f30
onlydcx <rin.hanaoka@icloud.com>
2025-01-07 15:32:14 +0000

    fix

--- launch/memory_publisher.launch.py
+++ launch/memory_publisher.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
     memory_publisher = launch_ros.actions.Node(
         package='mypkg',
         executable='memory_publisher',
-    ),
+    )
     listener = launch_ros.actions.Node(
         package='mypkg',
         executable='listener',
--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+
 timeout 10 ros2 launch mypkg memory_publisher.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
