50ace74e22ecc4b10bdb752a1c4857e4fb2b5b7f
onlydcx <rin.hanaoka@icloud.com>
2025-01-07 15:20:40 +0000

    fix

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'listener = mypkg.listener:main',
             'memory_publisher = mypkg.memory_publisher:main',
         ],
     },
